5eb390e45549a904b7eb5bb2168c48fa9049deb9
Jon Norton <jmichaeln5@gmail.com>
2021-09-24 19:26:35 -0400

    Adding functionality to Download Orders (CSV + XLS)

--- app/models/order.rb
+++ app/models/order.rb
@@ -17,15 +17,12 @@ class Order < ApplicationRecord
 
   #  before_save :before_save_methods
 
-  def self.to_csv
-    attributes = %w{ id po_number date_recieved }
-
-    CSV.generate(headers: true) do |csv|
-      csv << attributes
-
-      all.each do |contact|
-        csv << attributes.map{ |attr| contact.send(attr) }
-      end
+  def self.to_csv # Also Formats for XLS
+    CSV.generate do |csv|
+      csv << column_names
+        all.each do |order|
+          csv << order.attributes.values_at(*column_names)
+        end
     end
   end
 
