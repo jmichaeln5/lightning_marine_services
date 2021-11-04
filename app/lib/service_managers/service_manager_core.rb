3d75372797b8f90e379145230b92167ec7e99555
Jon Norton <jmichaeln5@gmail.com>
2021-11-03 22:34:20 -0400

    Defining ServiceManagers and nesting ServiceManagerCore mod, heroku/zeitwerk requires defining ServiceManagers::ServiceManagerCore

--- app/lib/service_managers/service_manager_core.rb
+++ app/lib/service_managers/service_manager_core.rb
@@ -1,7 +1,10 @@
-module ServiceManagerCore
+module ServiceManagers
+  module ServiceManagerCore
+    extend ServiceManagers # Heroku/Zeitwerk error: expected file /app/app/lib/service_managers/service_manager_core.rb to define constant ServiceManagers::ServiceManagerCore, but didn't (Zeitwerk::NameError) 
 
-  def init_service_manager( options={} ) # Set ivars to be used in when called in classes below
-    options.each { |k,v| instance_variable_set("@#{k}", v) }
+    def init_service_manager( options={} ) # Set ivars to be used in when called in classes below
+      options.each { |k,v| instance_variable_set("@#{k}", v) }
+    end
+
   end
-
 end
