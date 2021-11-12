60f08a7475d19f61e6a2ca76aa6d988599458d79
Jon Norton <jmichaeln5@gmail.com>
2021-11-11 22:59:48 -0500

    Removed unused User routes and changed page dropdown in top_table_header when 1/1 page to match with multiple pages

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
 
   get '/dashboard', to: 'dashboard#show', as: 'dashboard'
 
-  resources :users
+  # resources :users
   resources :purchasers
   resources :vendors
   resources :orders
