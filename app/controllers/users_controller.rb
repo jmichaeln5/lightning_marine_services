7bb742cefc4f556a9f9057c73f87d61a0ef75451
Jon Norton <jmichaeln5@gmail.com>
2021-11-11 22:59:48 -0500

    Removed unused User routes and changed page dropdown in top_table_header when 1/1 page to match with multiple pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,4 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :authenticate_admin, only: %i[ index ]
-  # GET /users or /users.json
-  def index
-
-    @users = User.all.order("created_at ASC")
-    users = User.all.order("created_at ASC")
-
-    # respond_to do |format|
-    #   format.html
-    #   format.csv do
-    #     headers['Content-Disposition'] = "attachment; filename=\"user-list\""
-    #     headers['Content-Type'] ||= 'text/csv'
-    #   end
-    # end
-
-
-  end
-
-  # GET /users/1 or /users/1.json
-  def show
-    @user = User.find(params[:id])
-    user = @user
-  end
-
+  before_action :authenticate_admin
 end
