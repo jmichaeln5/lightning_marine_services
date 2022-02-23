369340bdd7ca5d2f79866e296d381c1ec47f0fec
Jon Norton <jmichaeln5@gmail.com>
2022-02-23 00:16:19 -0500

    CI edit

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,8 @@ module LightningMarineServices
     #   config.autoloader = :classic
     # end
 
-    config.assets.initialize_on_precompile = false # Note from initial push to prod, Heroku is trying to connect to DB as part of 'rake assets:precompile' but cannot because DB has not been created yet
-    # config.assets.initialize_on_precompile = true # Note from initial push to prod, Heroku is trying to connect to DB as part of 'rake assets:precompile' but cannot because DB has not been created yet
+    # config.assets.initialize_on_precompile = false # Note from initial push to prod, Heroku is trying to connect to DB as part of 'rake assets:precompile' but cannot because DB has not been created yet
+    config.assets.initialize_on_precompile = true # Note from initial push to prod, Heroku is trying to connect to DB as part of 'rake assets:precompile' but cannot because DB has not been created yet
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
