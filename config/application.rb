ad59a80052e05986a447c70f9ce1b185b62d54eb
Jon Norton <jmichaeln5@gmail.com>
2022-02-23 00:09:04 -0500

    CI edit

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ Bundler.require(*Rails.groups)
 module LightningMarineServices
   class Application < Rails::Application
     config.load_defaults 6.0
+
+    if Rails.env.test?
+      config.autoloader = :classic
+    end
     # config.assets.initialize_on_precompile = false # Note from initial push to prod, Heroku is trying to connect to DB as part of 'rake assets:precompile' but cannot because DB has not been created yet
     config.assets.initialize_on_precompile = true # Note from initial push to prod, Heroku is trying to connect to DB as part of 'rake assets:precompile' but cannot because DB has not been created yet
 
