58dbc720c92d6334219faa7ada91294c19fae4be
Jon Norton <jmichaeln5@gmail.com>
2023-03-07 15:19:13 -0500

    Ensuring seeds executed in dev or test env only

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,14 @@ seed_dir_for_env = "db/seeds/#{Rails.env}"
 current_env_seeds_dir = Rails.root.join(seed_dir_for_env)
 env_has_seed_files =  Dir.exists? current_env_seeds_dir
 
-if env_has_seed_files
+def valid_env
+  current_env = Rails.env.upcase
+  return true if current_env == "DEVELOPMENT"
+  return true if current_env == "TEST"
+  return false
+end
+
+if valid_env && env_has_seed_files
   Dir.glob("#{current_env_seeds_dir}/*.rb") do |seed|
     load seed
   end
