57b68441e19fda77b3c559939c0a6b84b36e53f9
Jon Norton <jmichaeln5@gmail.com>
2023-03-13 20:22:44 -0400

    Adding stimulus-use pkg with yarn- Using in visibility_controller.js + added vvisibility#clickOutside() to close open el unless closeOnOutsideClick value === false

--- app/javascript/controllers/visibility_controller.js
+++ app/javascript/controllers/visibility_controller.js
@@ -1,29 +1,32 @@
 import { Controller } from "@hotwired/stimulus"
-
-// import { useClickOutside } from "stimulus-use";
-// yarn add stimulus-use
-// to close showing elements when clicked outside controller
+import { useClickOutside } from "stimulus-use";
 
 export default class extends Controller {
-  static values = { toggled: Boolean }
+  static values = {
+    toggled: Boolean,
+    closeOnOutsideClick: { type: Boolean, default: true }
+  }
   static targets = [ "hideable" ]
 
   connect() {
     console.log("visibility#connect()")
+    useClickOutside(this)
   };
 
   showTargets() {
     console.log("visibility#showTargets()")
     this.hideableTargets.forEach(el => {
       el.hidden = false
-    });
+    })
+    this.toggledValue = true
   };
 
   hideTargets() {
     console.log("visibility#hideTargets()")
     this.hideableTargets.forEach(el => {
       el.hidden = true
-    });
+    })
+    this.toggledValue = false
   };
 
   toggleTargets() {
@@ -35,19 +38,39 @@ export default class extends Controller {
     });
   };
 
-  toggledValueChanged(value, previousValue) {
-    // console.log("toggledValueChanged()")
-    if (previousValue != undefined) {
-      console.log("\n")
-      console.log("toggledValueChanged()")
-      // console.warn("toggledValueChanged()")
-      console.log("previousValue:")
-      console.log(previousValue)
-      console.log("\n")
+  // toggledValueChanged(value, previousValue) {
+  //   if (previousValue != undefined) {
+  //     console.log("visibility#toggleTargets()")
+  //     // console.warn("visibility#toggleTargets()")
+  //     console.log("previousValue:")
+  //     console.log(previousValue)
+  //     console.log("\n")
+  //
+  //     console.log("value:")
+  //     console.log(value)
+  //     console.log("\n")
+  //   }
+  // };
 
-      console.log("value:")
-      console.log(value)
-      console.log("\n")
+  clickOutside(event) {
+    console.log("visibility#clickOutside()")
+    if (this.data.get("clickOutside") === "add") {
+      this.toggleableTargets.forEach((target) => {
+        target.classList.add(target.dataset.cssClass);
+      })
+    } else if (this.data.get("clickOutside") === "remove") {
+      this.toggleableTargets.forEach((target) => {
+        target.classList.remove(target.dataset.cssClass);
+      })
+    }
+
+    if (this.closeOnOutsideClickValue != true) {
+      return
+    }
+
+    if (this.toggledValue === true) {
+      this.hideTargets()
     }
   };
-}
+
+};
