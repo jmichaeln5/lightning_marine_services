8f8f9881b299131401565cd0c93becc60c853ff4
Jon Norton <jmichaeln5@gmail.com>
2023-04-06 16:35:04 -0400

    Adding stacked_shell/headings dir + moving renamed partial (before: _header after: _page_heading) to new dir

--- app/models/concerns/order_parent.rb
+++ app/models/concerns/order_parent.rb
@@ -2,20 +2,13 @@ module OrderParent
   extend ActiveSupport::Concern
 
   included do
-    after_find do |order_parent|
-      establish_order_amount
-    end
-
     has_many :orders
-    attribute :order_amount, :integer
 
     before_destroy :check_associated_orders
   end
 
-  def establish_order_amount
-    self.order_ids.size # ?? Forgot, is this necessary?? Doesnt look like it
-    self.order_amount = self.order_ids.size
-    return self.order_amount
+  def order_amount
+    return self.order_ids.size
   end
 
   private
