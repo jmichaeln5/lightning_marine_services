21e6b1c79622f93c349ca8a50dc5a2fa9feb7ada
Jon Norton <jmichaeln5@gmail.com>
2023-04-07 04:20:43 -0400

    Adding new orders table to orders#all_orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,32 +13,103 @@ class OrdersController < ApplicationController
   before_action :set_pagination_params, only: %i[ all_orders ]
   helper_method :sort_option, :sort_direction
 
+  # def all_orders
+  #   load_resource_files
+  #
+  #   resource_attrs = {
+  #     called_at: Time.now,
+  #     user: current_user,
+  #     target: Order.all,
+  #     parent_class: Order,
+  #     parent_action: 'all_orders',
+  #     controller_name: 'orders',
+  #     controller_action: 'all_orders',
+  #     controller_name_and_action: 'orders#all_orders',
+  #     search_query: @query,
+  #     sort_option: sort_option,
+  #     sort_direction: sort_direction,
+  #     page: @page
+  #   }
+  #
+  #   @init_resource = Resource.init_resource_klass ( resource_attrs )
+  #   @resource = Resource::ResourceKlass.get_resource
+  #
+  #   @table_option = @resource.table_option
+  #   @orders = @resource.paginated_target
+  #   @order = Order.new
+  #   @order_content = @order != nil ? @order.build_order_content : OrderContent.new
+  # end
   def all_orders
-    load_resource_files
+    orders = Order.all
 
-    resource_attrs = {
-      called_at: Time.now,
-      user: current_user,
-      target: Order.all,
-      parent_class: Order,
-      parent_action: 'all_orders',
-      controller_name: 'orders',
-      controller_action: 'all_orders',
-      controller_name_and_action: 'orders#all_orders',
-      search_query: @query,
-      sort_option: sort_option,
-      sort_direction: sort_direction,
-      page: @page
-    }
+    if params[:query].present?
+      query_str = params[:query]
+      # results = orders.search(query_str).results
+      # results = orders.search(query_str, misspellings: { below: 3} ).results
+      results = orders.search(query_str, misspellings: { below: 6} ).results
+      results_arr = Array.new
+      results.each do |result|
+        results_arr << result.id if (result.archived? == false)
+      end
+      @orders = nil
+      # orders = Order.all.reorder('id ASC')
+      orders = orders.where(id: results_arr)
+    end
 
-    @init_resource = Resource.init_resource_klass ( resource_attrs )
-    @resource = Resource::ResourceKlass.get_resource
+    if params[:sort]
+      sort_col = %w{ id dept date_recieved courrier date_delivered }.include?(params[:sort]) ? params[:sort] : "id"
+      sort_dir = %w{ asc desc }.include?(params[:direction]) ? params[:direction] : "asc"
 
-    @table_option = @resource.table_option
-    @orders = @resource.paginated_target
+      if ((params[:sort] == "ship_name") || ( params[:sort] == "vendor_name" )) # None Order col
+        sorted_orders = Order.where(id: orders.ids).filter_by_purchasers(sort_dir) if params[:sort] == "ship_name"
+        sorted_orders = Order.where(id: orders.ids).filter_by_vendors(sort_dir) if params[:sort] == "vendor_name"
+        sorted_orders_ids = sorted_orders.ids
+        sorted_orders_ids_arr = Array.new
+
+        sorted_orders_ids.each do |order_id|
+          sorted_orders_ids_arr << order_id
+        end
+        # https://stackoverflow.com/a/61267426
+        order_query = <<-SQL
+          CASE orders.id
+            #{sorted_orders_ids_arr.map.with_index { |id, index| "WHEN #{id} THEN #{index}" } .join(' ')}
+            ELSE #{sorted_orders_ids_arr.length}
+          END
+        SQL
+
+        newly_sorted_orders = Order.where(id: sorted_orders_ids_arr).order(Arel.sql(order_query))
+        clear_active_record_query_cache
+      end
+
+      if ((params[:sort] != "ship_name") && ( params[:sort] != "vendor_name" )) # Order col
+        newly_sorted_orders = orders.reorder(sort_col => sort_dir)
+      end
+
+      @orders = nil
+      orders = nil
+      orders = newly_sorted_orders
+    end
+
+    @orders ||= orders
+    @pagy, @orders = pagy @orders, items: params.fetch(:count, 10)
+
     @order = Order.new
-    @order_content = @order != nil ? @order.build_order_content : OrderContent.new
+    @order.build_order_content
+
+    respond_to do |format|
+      format.html
+      format.csv { # give these formats a better home, shouldn't be in this controller or action
+        send_data (Order.all).to_csv,
+        filename: "Orders-#{(DateTime.now).try(:strftime,"%m/%d/%Y") }.csv",
+        type: 'text/csv; charset=utf-8'
+      }
+      format.xls { # give these formats a better home, shouldn't be in this controller or action
+        send_data (Order.all).to_csv, # method should be to_xls
+        filename: "LightningMarineServices_Orders-#{(DateTime.now).try(:strftime,"%m/%d/%Y") }.xls"
+      }
+    end
   end
+
 
   def index
     orders = Order.unarchived
