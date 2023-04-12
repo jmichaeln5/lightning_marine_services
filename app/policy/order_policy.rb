ee65ad12b6d5f73ade3f035f14457e7f1efe627b
Jon Norton <jmichaeln5@gmail.com>
2023-04-12 17:10:37 -0400

    Adding commented article link explaining policy object

--- app/policy/order_policy.rb
+++ app/policy/order_policy.rb
@@ -1,4 +1,5 @@
 # https://blog.eq8.eu/article/policy-object.html
+# https://www.honeybadger.io/blog/complete-guide-to-managing-user-permissions-in-rails-apps/#:~:text=Policy%20Object%20is%20a%20design,policy%20objects%20with%20different%20rules
 class OrderPolicy
   attr_reader :current_user, :resource
 
