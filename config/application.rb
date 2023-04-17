69d0a67d172bc62ba412e6f91b27e4be7bdd598b
Jon Norton <jmichaeln5@gmail.com>
2023-04-17 18:23:25 -0400

    Adding conditional to display btn

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,8 @@ Bundler.require(*Rails.groups)
 
 module LightningMarineServices
   class Application < Rails::Application
-    config.load_defaults 6.0
-
-    # if Rails.env.test?
-    #   config.autoloader = :classic
-    # end
+    config.load_defaults 6.0 # requires  -v 6.0 load_defaults when -v 6.1 ????
+    # config.load_defaults 6.1 # should replace above with this
 
     #################################### Migrating to esbuild
     # config.assets.initialize_on_precompile = false # Note from initial push to prod, Heroku is trying to connect to DB as part of 'rake assets:precompile' but cannot because DB has not been created yet
