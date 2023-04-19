c8194648ef81624671fa791ecb733556d4f309eb
Jon Norton <jmichaeln5@gmail.com>
2023-04-18 22:18:48 -0400

    Adding conditionals to ensure seeds file only ran in development or test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,32 @@
-seed_dir_for_env = "db/seeds/#{Rails.env}"
-current_env_seeds_dir = Rails.root.join(seed_dir_for_env)
-env_has_seed_files =  Dir.exists? current_env_seeds_dir
+current_env_str = Rails.env.to_s.downcase
 
-def valid_env
-  current_env = Rails.env.upcase
-  return true if current_env == "DEVELOPMENT"
-  return true if current_env == "TEST"
-  return false
+in_valid_seed_env = false
+valid_seed_env_str = false
+
+if ('development').in? current_env_str
+  in_valid_seed_env = true
+  valid_seed_env_str = 'development'
 end
 
-if valid_env && env_has_seed_files
+if ('test').in? current_env_str
+  in_valid_seed_env = true
+  valid_seed_env_str = 'test'
+end
+
+if !(valid_seed_env_str == 'development') || (valid_seed_env_str == 'test')
+  in_valid_seed_env = false
+  valid_seed_env_str = false
+end
+
+if in_valid_seed_env == true
+  seed_dir_for_env = "db/seeds/#{current_env_str}"
+  current_env_seeds_dir = Rails.root.join(seed_dir_for_env)
+  env_has_seed_files =  Dir.exists? current_env_seeds_dir
+
+  return unless (valid_seed_env_str == 'development') || (valid_seed_env_str == 'test')
+
   Dir.glob("#{current_env_seeds_dir}/*.rb") do |seed|
     load seed
   end
+
 end
