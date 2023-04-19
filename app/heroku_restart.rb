e4e83e9b085a59721d6f4bcd1c36fdd0bff31b39
Jon Norton <jmichaeln5@gmail.com>
2023-04-19 17:49:44 -0400

    Cleanup- removing 3 unused dirs+files

--- app/heroku_restart.rb
+++
@@ -1,9 +0,0 @@
-if Rails.env.development?
-  stop_dev_server = 'cat tmp/pids/server.pid | xargs -n1 -I pid kill -9 pid'
-  start_dev_server = 'rails s'
-  cmd = "#{stop_dev_server} && #{start_dev_server}"
-else
-  cmd = 'heroku restart -a lightningmarineservice'
-end
-
-system(cmd)
