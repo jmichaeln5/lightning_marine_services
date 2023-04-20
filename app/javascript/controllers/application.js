0ba768a61acbb8340366ccccdce621166be15e2c
Jon Norton <jmichaeln5@gmail.com>
2023-04-20 03:25:31 -0400

    Adding back stimulus-checkbox-select-all for dependency in stimulus-checkbox-select-all

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -3,12 +3,14 @@ import { Application } from "@hotwired/stimulus"
 import Dropdown from 'stimulus-dropdown'
 import Reveal from 'stimulus-reveal-controller'
 import Notification from 'stimulus-notification'
+import CheckboxSelectAll from 'stimulus-checkbox-select-all'
 
 const application = Application.start()
 // Register 3rd party packages here
 application.register('dropdown', Dropdown)
 application.register('reveal', Reveal)
 application.register('notification', Notification)
+application.register('checkbox-select-all', CheckboxSelectAll)
 
 // Configure Stimulus development experience
 application.debug = false
