94bff6299ed072cb3fc3ed21007ae33fff8ece0f
Jon Norton <jmichaeln5@gmail.com>
2023-04-20 02:58:37 -0400

    Reverting app/views/layouts/stacked_shell.html.erb + removing unused css files + adding default app layout

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,6 @@
 class DashboardController < ApplicationController
-  before_action :set_page_heading_title
-
   layout "stacked_shell"
+  before_action :set_page_heading_title
 
   #  # http://tutorials.jumpstartlab.com/topics/models/facade_pattern.html
   # Keep Controller Skinny!! By creating a new class for Dashboard in Models
