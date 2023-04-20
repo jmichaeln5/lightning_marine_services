ddede94ea47479a27fd8fc1d6e6f3971445cc5bf
Jon Norton <jmichaeln5@gmail.com>
2023-04-20 00:38:18 -0400

    Heroku build failed, error: SassC::SyntaxError: Error: Function rgb is missing argument  - Fixhttps://stackoverflow.com/a/72394790/8643768

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,9 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
+  ### Line below originally originally commented out # 4-20-23
   # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :nil # https://stackoverflow.com/a/72394790/8643768
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
